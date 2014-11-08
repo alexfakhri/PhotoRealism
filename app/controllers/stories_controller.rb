342cd226aa04088fb0567513a8c392c5cf75cf6b
alexfakhri <alexfakhri@me.com>
2014-11-08 17:51:50 +0000

    Creating story titles

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -4,4 +4,19 @@ class StoriesController < ApplicationController
     @stories = Story.all
   end
 
+  def new
+  	@story = Story.new
+  end
+
+  def create
+  	@story = Story.create(story_params)
+  	redirect_to '/stories'
+  end
+
+
+
+  def story_params
+  	params.require(:story).permit(:title)
+  end
+
 end
